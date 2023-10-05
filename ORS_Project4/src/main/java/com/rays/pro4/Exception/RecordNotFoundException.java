38a31a60713d542ea8a2089be57d1e13127677ef
namanmeena05 <meenanaman641@gmail.com>
2023-10-05 23:30:44 +0530

    demo

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rays.pro4.Exception;
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author Demo Project
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg){
+		super(msg);
+	}
+}
